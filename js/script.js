bca7d4c180325791821b16bfa94d6aae4b7ccd9c
Viktorino4ka <gnilickaya.v1989@icloud.com>
2020-05-27 11:01:32 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplaySpeed:3000,
+        autoplayHoverPause:true,
+
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+
+    });
+
+    AOS.init({
+        duration: 1200,
+    });
+});
+
+
+
+//Get the button:
+mybutton = document.getElementById("upBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+
